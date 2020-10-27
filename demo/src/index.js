91c8b04619e05f425b26d81a6d6e8eff27f2e629
Tarik Huber <huber.tarik@gmail.com>
2020-10-27 14:17:23 +0100

    Renamed rmw-shell-new to rmw-shell

---
+++ demo/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import { render } from 'react-dom'
+import App from '../../cra-template-rmw/template/src/App'
+
+render(<App />, document.querySelector('#root'))
---
+++ src/pages/CreateChat/index.js
@@ -0,0 +1,166 @@
+import React, { useEffect } from 'react'
+import { useLists } from 'rmw-shell/lib/providers/Firebase/Lists'
+import ListPage from 'material-ui-shell/lib/containers/Page/ListPage'
+import { useIntl } from 'react-intl'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemText from '@material-ui/core/ListItemText'
+import Divider from '@material-ui/core/Divider'
+import Avatar from '@material-ui/core/Avatar'
+import Mail from '@material-ui/icons/Mail'
+import Star from '@material-ui/icons/Star'
+import ListItemAvatar from '@material-ui/core/ListItemAvatar'
+import {
+  GoogleIcon,
+  FacebookIcon,
+  GitHubIcon,
+  TwitterIcon,
+} from 'rmw-shell/lib/components/Icons'
+import Badge from '@material-ui/core/Badge'
+import { useHistory } from 'react-router-dom'
+import { useAuth } from 'base-shell/lib/providers/Auth'
+
+const fields = [
+  {
+    name: 'displayName',
+    label: 'Name',
+  },
+]
+
+const getProviderIcon = (id) => {
+  const iconProps = {
+    color: 'primary',
+    style: {
+      height: 20,
+      width: 20,
+    },
+  }
+
+  if (id === 'google.com') {
+    return <GoogleIcon key={id} {...iconProps} />
+  }
+  if (id === 'facebook.com') {
+    return <FacebookIcon key={id} {...iconProps} />
+  }
+  if (id === 'github.com') {
+    return <GitHubIcon key={id} {...iconProps} />
+  }
+  if (id === 'twitter.com') {
+    return <TwitterIcon key={id} {...iconProps} />
+  }
+
+  return <Mail key={id} {...iconProps} />
+}
+
+export default function () {
+  const { firebaseApp, watchList, getList, isListLoading } = useLists()
+  const { auth } = useAuth()
+  const intl = useIntl()
+  const history = useHistory()
+
+  useEffect(() => {
+    watchList('users')
+    watchList('admins')
+  }, [watchList])
+
+  const admins = getList('admins')
+
+  const list = getList('users').map(({ key, val }) => {
+    return { key, ...val }
+  })
+
+  const handleRowClick = (user) => {
+    const key = user.key
+    const userValues = user
+    const userChatsRef = firebaseApp
+      .database()
+      .ref(`/user_chats/${auth.uid}/${key}`)
+
+    const chatData = {
+      displayName: userValues.displayName,
+      photoURL: userValues.photoURL ? userValues.photoURL : '',
+      lastMessage: '',
+    }
+
+    userChatsRef.update({ ...chatData })
+
+    history.push(`/chats/${key}`)
+  }
+
+  const Row = ({ data, index, style }) => {
+    const { displayName = '', key, photoURL, providerData = [] } = data
+
+    let isAdmin = false
+
+    admins.map((a) => {
+      if (a.key === key) {
+        isAdmin = true
+      }
+    })
+
+    return (
+      <div key={key} style={style}>
+        <ListItem
+          button
+          alignItems="flex-start"
+          style={{ height: 82 }}
+          onClick={() => handleRowClick(data)}
+        >
+          <ListItemAvatar>
+            <Badge
+              invisible={!isAdmin}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              badgeContent={
+                <Star
+                  style={{
+                    width: 15,
+                    padding: 0,
+                  }}
+                />
+              }
+              color="secondary"
+            >
+              <Avatar src={photoURL} style={{ height: 45, width: 45 }} />
+            </Badge>
+          </ListItemAvatar>
+
+          <ListItemText
+            primary={`${displayName}`}
+            secondary={
+              <React.Fragment>
+                {providerData.map((p) => {
+                  return getProviderIcon(p.providerId)
+                })}
+              </React.Fragment>
+            }
+          />
+        </ListItem>
+        <Divider variant="inset" />
+      </div>
+    )
+  }
+
+  return (
+    <ListPage
+      name="users"
+      list={list}
+      fields={fields}
+      Row={Row}
+      listProps={{ itemSize: 82 }}
+      getPageProps={(list) => {
+        return {
+          pageTitle: intl.formatMessage({
+            id: 'chat_with',
+            defaultMessage: 'Chat with',
+          }),
+          isLoading: isListLoading('users'),
+          onBackClick: () => {
+            history.goBack()
+          },
+        }
+      }}
+    />
+  )
+}
